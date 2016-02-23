27be334e72d79268cc4c8facc519ce01dedfb536
Will Kwon <will@kwon.co>
2016-02-23 14:36:33 -0800

    Changing LogInEmailActivity layout to reflect design

--- WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LogInEmailActivity.java
+++ WordPress/src/main/java/org/wordpress/android/ui/accounts/login/LogInEmailActivity.java
@@ -1,30 +1,14 @@
 package org.wordpress.android.ui.accounts.login;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+
 import org.wordpress.android.R;
 
 public class LogInEmailActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in_email_activity);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
