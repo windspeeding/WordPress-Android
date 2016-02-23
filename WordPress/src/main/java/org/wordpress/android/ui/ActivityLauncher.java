0526ddabf3d9ceae2a398db2f6f58cd7558368db
Will Kwon <will@kwon.co>
2016-02-23 14:36:51 -0800

    Opening LogInEmailActivity instead of SignInActivity

--- WordPress/src/main/java/org/wordpress/android/ui/ActivityLauncher.java
+++ WordPress/src/main/java/org/wordpress/android/ui/ActivityLauncher.java
@@ -18,6 +18,7 @@ import org.wordpress.android.ui.accounts.HelpActivity;
 import org.wordpress.android.ui.accounts.NewAccountActivity;
 import org.wordpress.android.ui.accounts.NewBlogActivity;
 import org.wordpress.android.ui.accounts.SignInActivity;
+import org.wordpress.android.ui.accounts.login.LogInEmailActivity;
 import org.wordpress.android.ui.comments.CommentsActivity;
 import org.wordpress.android.ui.main.SitePickerActivity;
 import org.wordpress.android.ui.prefs.MyProfileActivity;
@@ -226,7 +227,7 @@ public class ActivityLauncher {
     }
 
     public static void showSignInForResult(Activity activity) {
-        Intent intent = new Intent(activity, SignInActivity.class);
+        Intent intent = new Intent(activity, LogInEmailActivity.class);
         activity.startActivityForResult(intent, RequestCodes.ADD_ACCOUNT);
     }
 
